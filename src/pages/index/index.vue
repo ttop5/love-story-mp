c485f42b303044045508655fc7392f2eab090953
ttop5 <ttop5@qq.com>
2018-08-09 15:35:27 +0800

    chore: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>LOVE STORY</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+
+  methods: {},
+
+  mounted () {}
+}
+</script>
+
+<style scoped>
+</style>
